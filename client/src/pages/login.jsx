7f2c8c2d81f681328792ba07348decc9e49c3b8b
Goune <71518163+Goune1@users.noreply.github.com>
2024-05-20 16:24:21 +0200

    Update login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -70,12 +70,7 @@ export default function Example() {
           ) : (
             <>
               <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="../public/image/logo.png"
-              alt="Your Company"
-            />
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
               Se connecter
             </h2>
